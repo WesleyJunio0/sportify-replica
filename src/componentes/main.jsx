f1392a0f9999fb3db5a00be1c8b75115e111235f
Sleyd1 <211575227+WesleyJunio0@users.noreply.github.com>
2025-06-05 23:00:36 -0300

    estilizando a classe item-list, image button-image e textos

--- src/componentes/main.jsx
+++ src/componentes/main.jsx
@@ -7,21 +7,409 @@ const Main = () => {
   return (
     <div className="main">
         <div className="item__lista">
-          <div className="item-list__header">
+          <div className="item-list__header item-list__headerr ">
               <h2>Artista populares</h2>
               <a href="" className="main__link">Mostrar mais</a>
           </div>
-          <div className="single-item">
 
-              <div className="single-item__div-image">
+          <div className='item-list__container'>
+            <div className="single-item">
                 <div className='single-item__div-image-button'>
-                <img className='single-item__div-image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
-                <FontAwesomeIcon className="single-item__icon" icon={faCirclePlay} />
+                  <div className="single-item__div-image">
+            
+                    <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                  </div>
+            
+                    <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+            
+                  </div>
+                <div className='single-item__text'>
+                  <p className='single-item__text-title'>Avine Vinny</p>
+                  <p className='single-item__type'>Artista</p>
                 </div>
+            
+            </div>
+            <div className="single-item">
+                <div className='single-item__div-image-button'>
+                  <div className="single-item__div-image">
+            
+                    <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                  </div>
+            
+                    <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+            
+                  </div>
+                <div className='single-item__text'>
+                  <p className='single-item__text-title'>Avine Vinny</p>
+                  <p className='single-item__type'>Artista</p>
                 </div>
-              <p>Avine Vinny</p>
-              <p>Artista</p>
+            
+            </div>
+            <div className="single-item">
+                <div className='single-item__div-image-button'>
+                  <div className="single-item__div-image">
+            
+                    <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                  </div>
+            
+                    <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+            
+                  </div>
+                <div className='single-item__text'>
+                  <p className='single-item__text-title'>Avine Vinny</p>
+                  <p className='single-item__type'>Artista</p>
+                </div>
+            
+            </div>
+            <div className="single-item">
+                <div className='single-item__div-image-button'>
+                  <div className="single-item__div-image">
+            
+                    <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                  </div>
+            
+                    <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+            
+                  </div>
+                <div className='single-item__text'>
+                  <p className='single-item__text-title'>Avine Vinny</p>
+                  <p className='single-item__type'>Artista</p>
+                </div>
+            
+            </div>
+            <div className="single-item">
+                <div className='single-item__div-image-button'>
+                  <div className="single-item__div-image">
+            
+                    <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                  </div>
+            
+                    <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+            
+                  </div>
+                <div className='single-item__text'>
+                  <p className='single-item__text-title'>Avine Vinny</p>
+                  <p className='single-item__type'>Artista</p>
+                </div>
+            
+            </div>
+            <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+                <p className='single-item__text-title'>Avine Vinny</p>
+                <p className='single-item__type'>Artista</p>
+              </div>
               
+          </div>
+          <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+                <p className='single-item__text-title'>Avine Vinny</p>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
+          <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+                <p className='single-item__text-title'>Avine Vinny</p>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
+          <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+                <p className='single-item__text-title'>Avine Vinny</p>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
+          <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+                <p className='single-item__text-title'>Avine Vinny</p>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
+          <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+                <p className='single-item__text-title'>Avine Vinny</p>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
+          </div>
+        </div>
+
+
+        <div className="item__lista">
+          <div className="item-list__header item-list__headerr ">
+              <h2>Musicas populares</h2>
+              <a href="" className="main__link">Mostrar mais</a>
+          </div>
+
+          <div className='item-list__container'>
+            <div className="single-item">
+                <div className='single-item__div-image-button'>
+                  <div className="single-item__div-image">
+            
+                    <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                  </div>
+            
+                    <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+            
+                  </div>
+                <div className='single-item__text'>
+                  <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                  <p className='single-item__type'>Artista</p>
+                </div>
+            
+            </div>
+            <div className="single-item">
+                <div className='single-item__div-image-button'>
+                  <div className="single-item__div-image">
+            
+                    <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                  </div>
+            
+                    <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+            
+                  </div>
+                <div className='single-item__text'>
+                  <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                  <p className='single-item__type'>Artista</p>
+                </div>
+            
+            </div>
+            <div className="single-item">
+                <div className='single-item__div-image-button'>
+                  <div className="single-item__div-image">
+            
+                    <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                  </div>
+            
+                    <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+            
+                  </div>
+                <div className='single-item__text'>
+                 <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                  <p className='single-item__type'>Artista</p>
+                </div>
+            
+            </div>
+            <div className="single-item">
+                <div className='single-item__div-image-button'>
+                  <div className="single-item__div-image">
+            
+                    <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                  </div>
+            
+                    <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+            
+                  </div>
+                <div className='single-item__text'>
+                  <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                  <p className='single-item__type'>Artista</p>
+                </div>
+            
+            </div>
+            <div className="single-item">
+                <div className='single-item__div-image-button'>
+                  <div className="single-item__div-image">
+            
+                    <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                  </div>
+            
+                    <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+            
+                  </div>
+                <div className='single-item__text'>
+                  <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                  <p className='single-item__type'>Artista</p>
+                </div>
+            
+            </div>
+            <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+                <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
+          <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+               <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
+          <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+                <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
+          <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+               <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
+          <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+                <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
+          <div className="single-item">
+              <div className='single-item__div-image-button'>
+                <div className="single-item__div-image">
+                  
+
+                  <img className='single-item__image'  src="https://i.scdn.co/image/ab676161000051740673281edec44d5b28ca0f33" alt="imagem do cantor Avine Vinny" />
+                </div>
+                  
+                  <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+                
+                </div>
+              <div className='single-item__text'>
+               <div className='single-item__2line'>
+                    <p className='single-item__text-title'>Me Apaixonei Pela Pessoa Errada</p>
+                  </div>
+                <p className='single-item__type'>Artista</p>
+              </div>
+              
+          </div>
           </div>
         </div>
 
