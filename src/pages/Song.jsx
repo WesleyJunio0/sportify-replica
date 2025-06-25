56ee831e2d10664aa906449f3c93ccce00b9e0b9
Sleyd1 <211575227+WesleyJunio0@users.noreply.github.com>
2025-06-24 23:57:18 -0300

    Preparando a área do artista e musica

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Player from '../componentes/Player'
+import { Link } from 'react-router-dom'
+
 
 const Song = () => {
   return (
-    <div>Song</div>
+    <div className='song'>
+      <div className="song__container">
+        <div className='song__image-container'><img src="https://i.scdn.co/image/ab67616d00001e0277606320b7ae8d7f0fc43902" alt="" /></div>
+      </div>
+      <div className="song__bar">
+        <Link to="/artist/1" className='song__artist-image'>
+          <img width={75}
+          height={75} src="https://i.scdn.co/image/ab6761610000517449727bf298cb5fe23ab10154" alt="imagem do artista x" />
+        </Link>
+        <Player />
+        <div>
+          <p  className='song__name'>Baile de Favela</p>
+          <p>MC João</p>
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
