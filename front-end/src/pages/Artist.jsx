54db857b33f8c54cb7b72d07c4428e0e61ef2081
Sleyd1 <211575227+WesleyJunio0@users.noreply.github.com>
2025-06-30 13:27:33 -0300

    organizando minhas pasta do projeto

---
+++ front-end/src/pages/Artist.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Link, useParams } from "react-router-dom";
+import SongList from "../componentes/SongList.jsx";
+import { Artists } from "../database_banco_de_dados/Artists.js";
+import { songs } from "../database_banco_de_dados/songs.js";
+
+const Artist = () => {
+  const { id } = useParams();
+
+  const artistaObj = Artists.filter(
+    (currentArtistObj) => currentArtistObj._id === id
+  )[0];
+
+  const songsObj = songs.filter(
+    (currentsongObj) => currentsongObj.artist === artistaObj.name
+  );
+
+  const randomIndex = Math.floor(Math.random() * (songsObj.length - 1));
+
+  const randomIdfromArtist = songsObj[randomIndex]._id;
+
+  return (
+    <div className="artist">
+      <div
+        className="artist__header"
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(${artistaObj.banner})`,
+        }}
+      >
+        <h2 className="artist__title">{artistaObj.name}</h2>
+      </div>
+
+      <div className="artist__body">
+        <h2>Populares</h2>
+
+        <SongList songs={songsObj} />
+      </div>
+      <Link to={`/song/${randomIdfromArtist}`}>
+        <FontAwesomeIcon
+          className="single-item--artist  single-item__icon "
+          icon={faCirclePlay}
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Artist;
